847dd46672cdf3e5850b58ab6b31efa23f9f5dad
Super <1415942685@qq.com>
2019-03-02 18:15:28 +0800

    update code

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace k8s_sample.Controllers
+namespace hpaApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -13,8 +15,29 @@ namespace k8s_sample.Controllers
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
+        {   
+            var stringDistance = new StringDistance();
+            var  watch =new Stopwatch();
+            const long count = 100;
+            const int length = 100;
+            string comparestring = stringDistance.GenerateRandomString(length);
+            var strlist = new string[count];
+            var steps = new int[count];
+            // prepare string[] for comparison  
+
+            Parallel.For(0, count, i => strlist[i] = stringDistance.GenerateRandomString(length));
+            Console.WriteLine("已经生成了" + count + "个长度为" + length + "的字符串");
+
+            watch.Start();
+            for (int i = 0; i < count; i++)
+            {
+                steps[i] = stringDistance.LevenshteinDistance(comparestring, strlist[i]);
+            }
+            watch.Stop();
+            Console.WriteLine("完成非并行计算,耗时(ms)"+watch.ElapsedMilliseconds);
+            Console.WriteLine("性能比" + 100000d/watch.ElapsedMilliseconds);
+           
+            return Ok("ok");
         }
 
         // GET api/values/5
